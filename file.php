0513e7e03f0939412e174f00b293ed84075fc7eb
Glitch (fearless-visitor) <none>
2018-10-05 08:41:41 +0000

    ⛲️👩‍👩‍👦 Checkpoint ./file.php:1914278/248

---
+++ file.php
@@ -0,0 +1,5 @@
+<form method="POST" action="https://formspree.io/filip.dvorak13@gmail.com">
+  <input type="email" name="email" placeholder="Your email">
+  <textarea name="message" placeholder="Your message"></textarea>
+  <button type="submit">Send</button>
+</form>
