83da749fde31318a693ce1ee0cd7c6d467bac875
Glitch (fearless-visitor) <none>
2018-10-05 08:52:42 +0000

    ✌️📒 Checkpoint ./contact.html:1914278/611 ./products.html:1914278/482

--- file.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="https://formspree.io/filip.dvorak13@gmail.com">
-  <input type="email" name="email" placeholder="Your email">
-  <textarea name="message" placeholder="Your message"></textarea>
-  <button type="submit">Send</button>
-</form>
